a6c844be2638a058bac2517d00a6e2c53bd18e11
JerryWski <jarek_wasilewski@yahoo.com>
2024-01-02 22:39:44 +0100

    styling navbar and creating separate component for links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), icon()]
 });
--- src/data/navItems.ts
+++ src/data/navItems.ts
@@ -2,37 +2,37 @@ const navItems = [
     {
         text: 'Destinations',
         href: '#',
-        style: 'ghost'
+        
     },
     {
         text: 'Hotels',
         href: '#',
-        style: 'ghost'
+        
     },
     {
         text: 'Flights',
         href: '#',
-        style: 'ghost'
+        
     },
     {
         text: 'Bookings',
         href: '#',
-        style: 'ghost'
+        
     },
     {
         text: 'Login',
         href: '#',
-        style: 'ghost'
+        
     },
     {
         text: 'Sign up',
         href: '#',
-        style: 'outline'
+        
     },
     {
         text: 'EN',
         href: '#',
-        style: 'outline',
+        
         dropdown: true
     }
 ]
