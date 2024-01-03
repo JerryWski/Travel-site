282f7f81217f4ed35b7a221264fc15212fe9a298
JerryWski <jarek_wasilewski@yahoo.com>
2024-01-03 01:22:02 +0100

    styling and adding google fonts

--- src/data/navItems.ts
+++ src/data/navItems.ts
@@ -1,40 +1,34 @@
 const navItems = [
-    {
-        text: 'Destinations',
-        href: '#',
-        
-    },
-    {
-        text: 'Hotels',
-        href: '#',
-        
-    },
-    {
-        text: 'Flights',
-        href: '#',
-        
-    },
-    {
-        text: 'Bookings',
-        href: '#',
-        
-    },
-    {
-        text: 'Login',
-        href: '#',
-        
-    },
-    {
-        text: 'Sign up',
-        href: '#',
-        
-    },
-    {
-        text: 'EN',
-        href: '#',
-        
-        dropdown: true
-    }
-]
+  {
+    text: 'Destinations',
+    href: '#',
+  },
+  {
+    text: 'Hotels',
+    href: '#',
+  },
+  {
+    text: 'Flights',
+    href: '#',
+  },
+  {
+    text: 'Bookings',
+    href: '#',
+  },
+  {
+    text: 'Login',
+    href: '#',
+  },
+  {
+    text: 'Sign up',
+    href: '#',
+    className: 'sign-up',
+  },
+  {
+    text: 'EN',
+    href: '#',
+    dropdown: true,
+  },
+];
 
 export default navItems;
